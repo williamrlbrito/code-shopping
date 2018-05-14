22970b497b0331abe611152accfbc3b198926f9f
William <williamrldb@gmail.com>
2018-05-14 10:46:37 -0400

    --Trabalhando com slugs.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace CodeShopping\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'slug', 'active'];
+    use Sluggable;
+    protected $fillable = ['name', 'active'];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
