7b9d7b62fc32f7a962451065cb13fb1c49b721ea
William <williamrldb@gmail.com>
2018-05-21 11:48:44 -0400

    --Criando model Product.

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CodeShopping\Http\Controllers\Api;
+
+use CodeShopping\Http\Controllers\Controller;
+use CodeShopping\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Product $product)
+    {
+        //
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
