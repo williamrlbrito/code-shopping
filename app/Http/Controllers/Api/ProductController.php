c7b633637bf562402d39815e62a84a3bff5ddb08
William <williamrldb@gmail.com>
2018-05-21 12:11:55 -0400

    --Testando resource de produtos.

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,33 +3,41 @@
 namespace CodeShopping\Http\Controllers\Api;
 
 use CodeShopping\Http\Controllers\Controller;
+use CodeShopping\Http\Requests\ProductRequest;
 use CodeShopping\Models\Product;
-use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        //
+        return Product::all();
     }
 
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = Product::create($request->all());
+        $product->refresh();
+
+        return $product;
     }
 
     public function show(Product $product)
     {
-        //
+        return $product;
     }
 
-    public function update(Request $request, Product $product)
+    public function update(ProductRequest $request, Product $product)
     {
-        //
+        $product->fill($request->all());
+        $product->save();
+
+        return $product;
     }
 
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response([], 204);
     }
 }
---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace CodeShopping\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'active' => 'boolean'
+        ];
+    }
+}
