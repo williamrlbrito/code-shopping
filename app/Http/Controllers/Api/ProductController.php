df44012dd93560364e1f4ac3d72c2b976623162b
William <williamrldb@gmail.com>
2018-05-21 18:02:16 -0400

    --Iniciando com API Resource.

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,13 +4,14 @@ namespace CodeShopping\Http\Controllers\Api;
 
 use CodeShopping\Http\Controllers\Controller;
 use CodeShopping\Http\Requests\ProductRequest;
+use CodeShopping\Http\Resources\ProductResource;
 use CodeShopping\Models\Product;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return Product::paginate();
+        return ProductResource::collection(Product::paginate(10));
     }
 
     public function store(ProductRequest $request)
@@ -18,12 +19,12 @@ class ProductController extends Controller
         $product = Product::create($request->all());
         $product->refresh();
 
-        return $product;
+        return new ProductResource($product);
     }
 
     public function show(Product $product)
     {
-        return $product;
+        return new ProductResource($product);
     }
 
     public function update(ProductRequest $request, Product $product)
@@ -31,7 +32,7 @@ class ProductController extends Controller
         $product->fill($request->all());
         $product->save();
 
-        return $product;
+        return new ProductResource($product);
     }
 
     public function destroy(Product $product)
---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CodeShopping\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'price' => (float)$this->price,
+            'stock' => (int)$this->stock,
+            'active' => (boolean)$this->active,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
