82643674e7aa2b555361f29d6e0c8e87e1be2893
William <williamrldb@gmail.com>
2018-06-03 06:14:30 -0400

    --Capturando dados com with.

--- app/Models/ProductInput.php
+++ app/Models/ProductInput.php
@@ -10,4 +10,9 @@ class ProductInput extends Model
         'product_id',
         'amount'
     ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
