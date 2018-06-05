c751355036f18412434362347209553c9aa445e1
William <williamrldb@gmail.com>
2018-06-05 06:09:28 -0400

    --Regra personalizada para validar estoque na saída .

--- app/Http/Controllers/Api/ProductOutputController.php
+++ app/Http/Controllers/Api/ProductOutputController.php
@@ -3,9 +3,9 @@
 namespace CodeShopping\Http\Controllers\Api;
 
 use CodeShopping\Http\Controllers\Controller;
+use CodeShopping\Http\Requests\ProductOutputRequest;
 use CodeShopping\Http\Resources\ProductOutputResource;
 use CodeShopping\Models\ProductOutput;
-use Illuminate\Http\Request;
 
 class ProductOutputController extends Controller
 {
@@ -15,9 +15,10 @@ class ProductOutputController extends Controller
         return ProductOutputResource::collection($outputs);
     }
 
-    public function store(Request $request)
+    public function store(ProductOutputRequest $request)
     {
-        //
+        $output = ProductOutput::create($request->all());
+        return new ProductOutputResource($output);
     }
 
     public function show(ProductOutput $output)
