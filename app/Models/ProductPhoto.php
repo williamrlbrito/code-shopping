64eba8d2bfb421c35ea6e90d9c62f9e38075035e
William Brito <williamrldb@gmail.com>
2018-06-30 06:06:50 -0400

    --Criando registros das imagens.

--- app/Models/ProductPhoto.php
+++ app/Models/ProductPhoto.php
@@ -12,4 +12,10 @@ class ProductPhoto extends Model
     const PRODUCTS_PATH = self::BASE_PATH . '/' . self::DIR_PRODUCTS;
 
     protected $fillable = ['product_id', 'file_name'];
+
+    public static function photosPath($productSlug)
+    {
+        $path = self::PRODUCTS_PATH;
+        return storage_path("{$path}/{$productSlug}");
+    }
 }
--- database/seeds/ProductPhotosTableSeeder.php
+++ database/seeds/ProductPhotosTableSeeder.php
@@ -10,8 +10,10 @@ class ProductPhotosTableSeeder extends Seeder
     {
         $products = Product::all();
         $this->deleteAllPhotosInProductsPath();
-        $products->each(function($product){
-
+        $self = $this;
+        $products->each(function($product) use ($self) {
+            $self->createPhotoDir($product);
+            $self->createPhotosModels($product);
         });
     }
 
@@ -19,5 +21,26 @@ class ProductPhotosTableSeeder extends Seeder
     {
         $path = ProductPhoto::PRODUCTS_PATH;
         \File::deleteDirectory(storage_path($path), true);
+    }
+
+    private function createPhotoDir(Product $product)
+    {
+        $path = ProductPhoto::photosPath($product->slug);
+        \File::makeDirectory($path, 0777, true);
+    }
+
+    private function createPhotosModels(Product $product)
+    {
+        foreach (range(1, 5) as $v) {
+            $this->createPhotoModel($product);
+        }
+    }
+
+    private function createPhotoModel(Product $product)
+    {
+        ProductPhoto::create([
+            'product_id' => $product->id,
+            'file_name' => 'imagem.jpg'
+        ]);
     }
 }
