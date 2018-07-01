23a8626ee1fe51187d2f90267d209a8ca76e9201
William Brito <williamrldb@gmail.com>
2018-07-01 06:56:17 -0400

    --Listando registros das imagens.

---
+++ app/Http/Controllers/Api/ProductPhotoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace CodeShopping\Http\Controllers\Api;
+
+use CodeShopping\Http\Controllers\Controller;
+use CodeShopping\Models\ProductPhoto;
+use CodeShopping\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductPhotoController extends Controller
+{
+    public function index(Product $product)
+    {
+        return $product->photos;
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Product $product, ProductPhoto $photo)
+    {
+        if ($photo->product_id != $product->id) {
+            abort(404);
+        }
+        return $photo;
+    }
+
+    public function update(Request $request, ProductPhoto $productPhoto)
+    {
+        //
+    }
+
+    public function destroy(ProductPhoto $productPhoto)
+    {
+        //
+    }
+}
