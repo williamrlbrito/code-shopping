b5d097bcbe106d79e3fda0251237dbccd14e742b
William Brito <williamrldb@gmail.com>
2018-07-31 05:42:36 -0400

    --Regra de validação para unicidade de registro.

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,7 +25,7 @@ class UserRequest extends FormRequest
     {
         return [
             'name' => 'required|max:255',
-            'email' => 'required|max:255|email',
+            'email' => 'required|max:255|email|unique:users,email',
             'password' => 'required|min:4|max:16'
         ];
     }
