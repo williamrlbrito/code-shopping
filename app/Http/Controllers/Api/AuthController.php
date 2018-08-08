c13cbabfb485717607a858f7a0271e36b531f95a
William Brito <williamrldb@gmail.com>
2018-08-08 06:01:23 -0400

    --Criando operação de refresh token.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -35,4 +35,10 @@ class AuthController extends Controller
         $user = \Auth::guard('api')->user();
         return new UserResource($user);
     }
+
+    public function refresh()
+    {
+        $token = \Auth::guard('api')->refresh(); 
+        return ['token' => $token];   
+    }
 }
