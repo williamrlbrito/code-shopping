c0718312ed61dda5bf91438fa1b4b339739c7fd0
William Brito <williamrldb@gmail.com>
2018-08-08 05:42:24 -0400

    --Pegando usuário autenticado.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace CodeShopping\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use CodeShopping\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use CodeShopping\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
@@ -27,5 +28,11 @@ class AuthController extends Controller
     {
         \Auth::guard('api')->logout();
         return response()->json([], 204);
+    }
+
+    public function me()
+    {
+        $user = \Auth::guard('api')->user();
+        return new UserResource($user);
     }
 }
