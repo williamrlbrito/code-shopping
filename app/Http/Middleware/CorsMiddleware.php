f5e706e97b0f829035c527fab534ef7cba293d9b
William Brito <williamrldb@gmail.com>
2018-08-14 08:10:48 -0400

    --Configuração do CORS.

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CodeShopping\Http\Middleware;
+
+use Closure;
+
+class CorsMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        if ($request->is('api/*')) {
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Headers: Content-Type');
+        }
+        return $next($request);
+    }
+}
