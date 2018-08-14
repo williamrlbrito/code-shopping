cd0dea9e8c452767c56cdf9e34079aac805c7924
William Brito <williamrldb@gmail.com>
2018-08-14 04:58:08 -0400

    --Two way data binding.

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/pages/login/login.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { LoginComponent } from './components/pages/login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
